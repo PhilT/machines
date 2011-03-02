491a3f7509b58f8488421c67ed938f7ee2423369
Phil Thompson <phil@electricvisions.com>
2011-03-02 21:47:34 +0000

    Added a command to generate apache/nginx passwords (for basic auth). Moved app configuration to apps package including ssl because it's app specific. Moved dev packages to the end of Machinesfile. Made a few more things configurable.

---
+++ lib/packages/apps.rb
@@ -0,0 +1,46 @@
+require 'erb'
+require 'ostruct'
+class AppBuilder < OpenStruct
+  def get_binding
+    binding
+  end
+end
+
+AppConf.user.home = File.join('/home', AppConf.user.name)
+
+yaml = YAML.load(File.open('apps.yml'))
+AppConf.apps = []
+yaml.each do |app_name, app_hash|
+  app = AppBuilder.new
+  app.name = app_name
+  app.path = File.join(AppConf.appsroot, app.path)
+  app.ssl_key = app_hash[AppConf.environment].ssl + '.key'
+  app.ssl_crt = app_hash[AppConf.environment].ssl + '.crt'
+  app_hash.each do |k, v|
+    app[k] = v unless v.is_a?(Hash)
+  end
+
+  app_hash[AppConf.environment].each do |k, v|
+    app[k] = v
+  end
+  AppConf.apps << app
+end
+
+def generate_template_for(app, enable_ssl = false)
+  app.enable_ssl = enable_ssl
+  path = File.join(AppConf[AppConf.webserver].path, AppConf.app_servers, "#{app.name}_#{enable_ssl ? 'ssl' : ''}.conf"
+  template File.join(AppConf.webserver, 'app_server.conf.erb'), :settings => app, :to => path)
+end
+
+mkdir File.join(AppConf.nginx.path, AppConf.app_servers)
+AppConf.apps.each do |app|
+  make_app_structure app.path # check this is needed for all environments
+  generate_template_for(app)
+  if app.ssl_key
+    generate_template_for(app, true)
+    upload "certificates/#{ssl_crt}", '/etc/ssl/certs/{ssl_crt}'
+    upload "certificates/#{ssl_key}", '/etc/ssl/private/#{ssl_key}'
+  end
+  write_database_yml app, File.join(app.path, 'shared', 'config')
+end
+
