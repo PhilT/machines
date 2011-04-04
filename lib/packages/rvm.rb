359b3d098cb1fbea21cd339742ec8c4480ae816b
Phil Thompson <phil.thompson@groupm.com>
2011-04-04 13:45:22 +0100

    added no prompting for new .rvmrc files

--- lib/packages/rvm.rb
+++ lib/packages/rvm.rb
@@ -1,6 +1,8 @@
+# Install RVM and turn off trust prompting for new .rvmrc files
+
 sudo install 'curl'
 dir = "#{AppConf.user.home}/.rvm/src/rvm"
 install AppConf.rvm.url, :to => dir
 run "source #{AppConf.user.home}/.rvm/scripts/rvm"
-#run "rvm reload", :as => username    #may not be needed
+run append 'export rvm_trust_rvmrcs_flag=1', :to => '#{AppConf.user.home}/.rvmrc'
 
