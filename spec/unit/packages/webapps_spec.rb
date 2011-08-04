7f9038d6949b68e3182704b0e9cb318101d2bf8c
Phil Thompson <phil.thompson@groupm.com>
2011-08-04 11:19:28 +0100

    modified spec to handle differences in YAML implementations

--- spec/unit/packages/webapps_spec.rb
+++ spec/unit/packages/webapps_spec.rb
@@ -11,7 +11,7 @@ describe 'packages/webapps' do
     load_package('webapps')
 
     AppConf.from_hash(:user => {:name => 'username', :home => 'home_dir'}, :ruby => {:version => 'ruby_version'})
-    AppConf.apps = {'application' => AppBuilder.new('name' => 'application', 'path' => 'app_path', 'enable_ssl' => nil)}
+    AppConf.apps = {'application' => AppBuilder.new('name' => 'application', 'path' => 'app_path', 'enable_ssl' => nil, 'db_password' => 'pa$$')}
     AppConf.webserver = 'nginx'
     AppConf.from_hash(:nginx => {:path => 'nginx_path', :servers_dir => 'servers'})
     AppConf.from_hash(:db => {:address => 'db_master'})
@@ -22,23 +22,23 @@ describe 'packages/webapps' do
   it 'adds the following commands' do
     AppConf.environment = :production
     eval_package
-    AppConf.commands.map(&:info).should == [
+    AppConf.commands.map(&:info).map{|info| info.gsub(" \n", "\n")}.should == [
       "RUN    mkdir -p nginx_path/servers",
       "RUN    mkdir -p app_path/releases",
       "RUN    mkdir -p app_path/shared/config",
       "RUN    mkdir -p app_path/shared/system",
       "RUN    echo \"the template\n\" > nginx_path/servers/application.conf",
-      "RUN    echo \"--- \nproduction: \n  adapter: mysql\n  database: application\n  username: application\n  password: \n  host: db_master\n  encoding: utf8\n\" > app_path/shared/config/database.yml"
+      "RUN    echo \"---\nproduction:\n  adapter: mysql\n  database: application\n  username: application\n  password: pa$$\n  host: db_master\n  encoding: utf8\n\" > app_path/shared/config/database.yml"
     ]
   end
 
   it "doesn't make app structure when target is a development machine" do
     AppConf.environment = :development
     eval_package
-    AppConf.commands.map(&:info).should == [
+    AppConf.commands.map(&:info).map{|info| info.gsub(" \n", "\n")}.should == [
       "RUN    mkdir -p nginx_path/servers",
       "RUN    echo \"the template\n\" > nginx_path/servers/application.conf",
-      "RUN    echo \"--- \ndevelopment: \n  adapter: mysql\n  database: application\n  username: application\n  password: \n  host: db_master\n  encoding: utf8\n\" > app_path/shared/config/database.yml"
+      "RUN    echo \"---\ndevelopment:\n  adapter: mysql\n  database: application\n  username: application\n  password: pa$$\n  host: db_master\n  encoding: utf8\n\" > app_path/shared/config/database.yml"
     ]
   end
 end
