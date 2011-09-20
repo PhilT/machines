3fd4303399b87b7d3b650845a39ca731d446b60d
Phil Thompson <phil@example.com>
2011-09-20 09:15:09 +0100

    fixed text change causing spec fail

--- spec/lib/packages/unison_spec.rb
+++ spec/lib/packages/unison_spec.rb
@@ -8,7 +8,7 @@ describe 'packages/unison' do
   it 'adds the following commands' do
     eval_package
     AppConf.commands.map(&:info).should == [
-      "TASK   unison - Install unison two way file sync and set it to run hourly",
+      "TASK   unison - Install unison two way file sync and set it to run hourly. Config in users/user/.unison/default.prf",
       "SUDO   apt-get -q -y install unison",
       "SUDO   ln -sf /usr/bin/unison /etc/cron.hourly"
     ]
