5598ef88e95528cb5845fc4b7e9e3d2d9fc2e099
Phil Thompson <phil@electricvisions.com>
2011-11-07 23:52:23 +0000

    Fixed a few missed AppConf key changes.

--- lib/packages/passenger_nginx.rb
+++ lib/packages/passenger_nginx.rb
@@ -1,7 +1,7 @@
 task :passenger_nginx, 'Install Passenger Nginx module' do
-  file_check = check_file(File.join(AppConf.nginx.path, 'sbin', 'nginx'))
-  prefix = "--prefix=#{AppConf.nginx.path}"
-  source_dir = "--nginx-source-dir=/tmp/nginx-#{AppConf.nginx.version}"
+  file_check = check_file(File.join(AppConf.webserver.path, 'sbin', 'nginx'))
+  prefix = "--prefix=#{AppConf.webserver.path}"
+  source_dir = "--nginx-source-dir=/tmp/nginx-#{AppConf.webserver.version}"
   flags = "--extra-configure-flags=--with-http_ssl_module"
   run "echo #{AppConf.password} | rvmsudo -S passenger-install-nginx-module --auto #{prefix} #{source_dir} #{flags}", file_check
 end
--- spec/lib/packages/passenger_nginx_spec.rb
+++ spec/lib/packages/passenger_nginx_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'packages/passenger_nginx' do
   before(:each) do
     load_package('passenger_nginx')
-    AppConf.from_hash(:nginx => {:path => 'nginx_dest', :version => '1.0.2'})
+    AppConf.from_hash(:webserver => {:path => 'nginx_dest', :version => '1.0.2'})
     AppConf.password = 'pass'
   end
 
