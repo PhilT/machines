9b13be7e5813d5c4430d49f9b0d88579b5dc0ec5
Phil Thompson <phil@electricvisions.com>
2011-12-27 10:24:11 +0000

    Forgot to add files

---
+++ lib/packages/rbenv.rb
@@ -0,0 +1,21 @@
+task :rbenv, "Install ruby-build, rbenv, ruby #{AppConf.ruby.version} and Bundler" do
+  sudo install ['git-core', 'curl']
+  run git_clone 'git://github.com/sstephenson/ruby-build.git'
+  sudo 'cd ~/ruby-build && ./install.sh'
+
+  run git_clone 'git://github.com/sstephenson/rbenv.git', :to => '~/.rbenv'
+  run append 'export PATH="$HOME/.rbenv/bin:$PATH"', :to => '~/.bashrc'
+  run append '[ -f ~/.bundler-exec.sh ] && source ~/.bundler-exec.sh', :to => '~/.bashrc'
+  run append 'eval "$(rbenv init -)"', :to => '~/.bashrc'
+  run 'source ~/.bashrc'
+
+  run "rbenv install #{AppConf.ruby.full_version}"
+  run 'rbenv rehash'
+  run "rbenv global #{AppConf.ruby.full_version}", "ruby -v | grep #{AppConf.ruby.version} #{echo_result}"
+
+  run write "gem: --no-rdoc --no-ri", :to => '.gemrc', :name => '.gemrc'
+  run gem 'bundler'
+  run 'curl -L https://github.com/gma/bundler-exec/raw/master/bundler-exec.sh > ~/.bundler-exec.sh'#, file_check('~/.bundler-exec.sh')
+  run 'source ~/.bashrc', "type ruby | grep run-with-bundler #{echo_result}"
+end
+
---
+++ spec/lib/packages/rbenv_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'packages/rbenv' do
+  before(:each) do
+    load_package('rbenv')
+    AppConf.ruby = AppConf.new
+    AppConf.ruby.version = '1.9.2'
+    AppConf.ruby.full_version = '1.9.2-p290'
+  end
+
+  it 'adds the following commands' do
+    eval_package
+    AppConf.commands.map(&:info).should == [
+      'TASK   rbenv - Install ruby-build, rbenv, ruby 1.9.2 and Bundler',
+      "SUDO   apt-get -q -y install git-core",
+      "SUDO   apt-get -q -y install curl",
+      'RUN    git clone -q git://github.com/sstephenson/ruby-build.git',
+      'SUDO   cd ~/ruby-build && ./install.sh',
+      'RUN    git clone -q git://github.com/sstephenson/rbenv.git ~/.rbenv',
+      'RUN    echo "export PATH=\\"\\$HOME/.rbenv/bin:\\$PATH\\"" >> ~/.bashrc',
+      'RUN    echo "[ -f ~/.bundler-exec.sh ] && source ~/.bundler-exec.sh" >> ~/.bashrc',
+      'RUN    echo "eval \\"\\$(rbenv init -)\\"" >> ~/.bashrc',
+      'RUN    source ~/.bashrc',
+
+      'RUN    rbenv install 1.9.2-p290',
+      'RUN    rbenv rehash',
+      'RUN    rbenv global 1.9.2-p290',
+
+      'UPLOAD buffer from .gemrc to .gemrc',
+      'RUN    gem install bundler',
+      'RUN    curl -L https://github.com/gma/bundler-exec/raw/master/bundler-exec.sh > ~/.bundler-exec.sh',
+      'RUN    source ~/.bashrc'
+    ]
+  end
+end
+
