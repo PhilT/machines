a7f8f5a9b57eb403a21acdd666576eacaada2649
Phil Thompson <phil@electricvisions.com>
2012-05-21 14:46:12 +0100

    Fix upstart script so Nginx is correctly shutdown

--- lib/packages/base.rb
+++ lib/packages/base.rb
@@ -2,7 +2,7 @@ task :base, 'Install base packages for compiling Ruby and libraries' do
   sudo install [
     'build-essential',     # Needed to build most libraries for Ruby
     'zlib1g-dev',          # Compression library
-    'libpcre3-dev',        # Regular expressions library
+    'libpcre3-dev',        # Needed by Nginx
     'ruby1.9.1-dev',       # Needed by Ruby
     'libreadline-dev',     # Needed by IRB
     'libxml2-dev',         # Needed by Nokogiri
