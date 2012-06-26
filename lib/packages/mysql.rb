c51c07df863696f0d81711969e8eea44ecfff9f1
Phil Thompson <phil@electricvisions.com>
2012-06-26 20:30:42 +0100

    corrected mysql service name

--- lib/packages/mysql.rb
+++ lib/packages/mysql.rb
@@ -3,23 +3,22 @@ def mysql_execute(sql, options)
   run "echo \"#{sql}\" | mysql -u root -p#{options[:password]}", nil
 end
 
-only :roles => :db do
+only roles: :db do
   task :mysql, 'Install MySQL' do
     name = 'mysql-server-5.5'
     key = 'mysql-server/root_password'
     sudo debconf name, key, 'password', $conf.machine.root_pass
     sudo debconf name, "#{key}_again", 'password', $conf.machine.root_pass
     sudo install %w(mysql-server mysql-client libmysqlclient-dev)
-    run restart 'mysqld'
+    run restart 'mysql'
   end
 end
 
-only :roles => :dbmaster do
+only roles: :dbmaster do
   task :dbperms, 'Grant applications access to the database' do
     $conf.webapps.values.each do |app|
       mysql_execute "GRANT ALL ON *.* TO '#{app.name}'@'%' " +
-        "IDENTIFIED BY '#{app.password}';",
-        :password => $conf.machine.root_pass
+        "IDENTIFIED BY '#{app.password}';", password: $conf.machine.root_pass
     end
   end
 
@@ -27,23 +26,21 @@ only :roles => :dbmaster do
     sudo upload "mysql/dbmaster.cnf", "/etc/mysql/conf.d/dbmaster.cnf"
     mysql_execute "GRANT REPLICATION SLAVE ON *.* " +
       "TO '#{$conf.machine.replication_user}'@'%' " +
-      "IDENTIFIED BY '#{$conf.machine.replication_pass}';",
-      :password => $conf.machine.root_pass
+      "IDENTIFIED BY '#{$conf.machine.replication_pass}';", password: $conf.machine.root_pass
   end
 end
 
-only :roles => :dbslave do
+only roles: :dbslave do
   task :replication, 'Setup database replication from master' do
     sudo upload "mysql/dbslave.cnf", "/etc/mysql/conf.d/dbslave.cnf"
     mysql_execute "CHANGE MASTER TO " +
       "MASTER_HOST='#{$conf.db_server.address}', " +
       "MASTER_USER='#{$conf.db_server.replication_user}' " +
-      "MASTER_PASSWORD='#{$conf.db_server.replication_pass}';",
-    :password => $conf.machine.root_pass
+      "MASTER_PASSWORD='#{$conf.db_server.replication_pass}';", password: $conf.machine.root_pass
   end
 end
 
-task :monit_mysql, 'Configure monit for MySQL', :if => [:monit, :mysql] do
-  sudo create_from 'monit/conf.d/mysql.erb', :to => '/etc/monit/conf.d/mysql'
+task :monit_mysql, 'Configure monit for MySQL', if: [:monit, :mysql] do
+  sudo create_from 'monit/conf.d/mysql.erb', to: '/etc/monit/conf.d/mysql'
 end
 
