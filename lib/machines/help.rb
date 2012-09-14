c2626e302e9b5f89038800d2410aa67b11ecf772
Phil Thompson <phil@electricvisions.com>
2012-09-14 11:00:34 +0100

    `machines build` lists machines from machines.yml

--- lib/machines/help.rb
+++ lib/machines/help.rb
@@ -4,9 +4,9 @@ module Machines
       @actions = {
         'htpasswd' => 'Generates basic auth in webserver/conf/htpasswd',
         'new <DIR>' => 'Generates an example machines project in DIR',
-        'dryrun <machine>' => 'Logs commands but does not run them',
+        'dryrun [machine]' => 'Logs commands but does not run them. Lists machines if none specified',
         'tasks' => 'Lists the available tasks',
-        'build <machine> [task]' => 'Builds your chosen machine. Optionally, build just one task',
+        'build [machine] [task]' => 'Builds your chosen machine. Optionally, build just one task. Lists machines if none specified',
         'list' => 'Lists the available machines',
         'packages' => 'Lists the available packages',
         'override <PACKAGE>' => 'Copies the default package into project/packages so it can be edited/overidden'
@@ -17,8 +17,18 @@ module Machines
       @actions.keys.map{|key| key.gsub(/ .*/, '')}
     end
 
+    def machine_list
+      $conf.machines = AppConf.new
+      $conf.load('machines.yml')
+      <<-LIST
+machines build MACHINE
+MACHINE can be one of:
+#{$conf.machines.keys.map{|machine| "  #{machine}" }.join("\n")}
+LIST
+    end
+
     def syntax
-      <<HELP
+      <<-HELP
 machines v#{Machines::VERSION} - Ubuntu/Ruby configuration tool.
 machines COMMAND
 COMMAND can be:
--- lib/machines/version.rb
+++ lib/machines/version.rb
@@ -1,4 +1,4 @@
 module Machines
-  VERSION = '0.5.2'
+  VERSION = '0.5.3'
 end
 
